b78e2208ee25094f740e1614b6d7181f04693fad
FFarquar <110162452+FFarquar@users.noreply.github.com>
2024-12-08 11:08:17 +1100

    1.96. Added in summary view to see all important stats in one view. Still some styling to do here

--- Models/BroadsideSDS.cs
+++ Models/BroadsideSDS.cs
@@ -4,12 +4,79 @@ namespace ShipSelector.Models
 {
     public class BroadsideSDS
     {
+        private int hullhits;
+        public int HullHits {
+            get
+            {
+                return hullhits;
+            }
+            set
+            {
+                hullhits = value;
+                setSummaryText();
+            }
+        }
 
-        public string ShipName { get; set; }
+        private int floodingHits;
+
+        public int FloodingHits
+        {
+            get { return floodingHits; }
+            set { 
+                floodingHits = value;
+                setSummaryText();
+            }
+        }
+
+        private string shipName;
+
+        public string ShipName
+        {
+            get { return shipName; }
+            set { 
+                shipName = value;
+                setSummaryText() ;
+            }
+        }
+
+        private int currentSpeed;
+
+        public int CurrentSpeed
+        {
+            get { return currentSpeed; }
+            set { 
+                currentSpeed = value;
+                setSummaryText();
+            }
+        }
+
+        //private List<DamageCard> damageCards= new List<DamageCard>();
+
+        //public List<DamageCard> DamageCards
+        //{
+        //    get { return damageCards; }
+        //    set { 
+        //        damageCards = value;
+        //        setSummaryText();
+        //    }
+        //}
+
+        private string directedGunTag;
+
+        public string DirectedGunTag
+        {
+            get { return directedGunTag; }
+            set { 
+                directedGunTag = value;
+                setSummaryText();
+            }
+        }
+
+        //public string ShipName { get; set; }
         public string ImagePath { get; set; }
-        public int CurrentSpeed { get; set; } = 0;
-        public int HullHits { get; set; } = 0;
-        public int FloodingHits { get; set; } = 0;
+        //public int CurrentSpeed { get; set; } = 0;
+
+        //public int FloodingHits { get; set; } = 0;
         public List<DamageCard> DamageCards { get; set; } = new List<DamageCard>();
         public int selectedOrderCard { get; set; } = 0;
         public string selectedOrderImagePath { get; set; } = string.Empty;
@@ -18,6 +85,12 @@ namespace ShipSelector.Models
         public bool starTorpsFired { get; set; } = false;
         public bool portTorpsFired { get; set; } = false;
         public bool renameHidden { get; set; } = true;
+        public string summaryText { get; set; } = string.Empty;
+
+        public void setSummaryText()
+        {
+            summaryText = "S:" + CurrentSpeed.ToString() + " HH:" + HullHits.ToString() +" F:" + FloodingHits.ToString() + " DC:" + DamageCards.Count.ToString() + " ID:" + DirectedGunTag;     
+        }
     }
 
     public class DamageCardData
